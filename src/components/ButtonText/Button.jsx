c4a5d881bde3ffa7d8c896867e2434ee8baed9e4
郭建强 <guojianqiang@guojianqiangdeMacBook-Pro.local>
2020-05-19 20:52:50 +0800

    测试

--- src/components/ButtonText/Button.jsx
+++ src/components/ButtonText/Button.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import {Button} from 'antd'
-// import 'antd/dist/antd.css'
-import './style.scss'
-export default class ButtonText extends Component {
 
-  render () {
+export default class Button extends Component {
+  render() {
     return (
-      <Button className='test' type='primary'>确定</Button>
+      <div>
+        
+      </div>
     )
   }
 }
+
